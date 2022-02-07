05940dc352bef6266537ba8d6e0893e6e8175c0c
Ahmed Ibrahim <ib153507@gmail.com>
2022-02-07 14:48:07 +0200

    Intercept backend response

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,1 +1,18 @@
 /// <reference types="cypress" />
+
+const URL = "https://www.demoblaze.com/index.html";
+describe("Homepage content", () => {
+  beforeEach(() => {
+    cy.visit(URL);
+    cy.fixture("products").then((products) => {
+      console.log(products);
+      cy.intercept("/entries", products);
+    });
+  });
+
+  it("Procusts counts should be 9", () => {
+    cy.get(".card").should("have.length", 9);
+  });
+
+  it.only("Check product card content", () => {});
+});
