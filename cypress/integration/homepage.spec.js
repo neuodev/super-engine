964ec704aa4b9875239e1f17b82651b25feeb0ae
Ahmed Ibrahim <ib153507@gmail.com>
2022-02-07 14:53:50 +0200

    Check for card content

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -4,15 +4,25 @@ const URL = "https://www.demoblaze.com/index.html";
 describe("Homepage content", () => {
   beforeEach(() => {
     cy.visit(URL);
-    cy.fixture("products").then((products) => {
-      console.log(products);
-      cy.intercept("/entries", products);
-    });
   });
 
   it("Procusts counts should be 9", () => {
     cy.get(".card").should("have.length", 9);
   });
 
-  it.only("Check product card content", () => {});
+  it("Check product card content", () => {
+    cy.fixture("products").then((products) => {
+      cy.intercept("/entries", products);
+    });
+    cy.get(".card:first-child")
+      .get('img[src="imgs/galaxy_s6.jpg"]')
+      .should("exist");
+    cy.get(".card:first-child").contains("Samsung galaxy s6").should("exist");
+    cy.get(".card:first-child").contains("$360").should("exist");
+    cy.get(".card:first-child")
+      .contains(
+        "The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos"
+      )
+      .should("exist");
+  });
 });
