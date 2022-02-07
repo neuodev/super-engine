28f44817eb6e2993c725f623760e695504a8c748
Ahmed Ibrahim <ib153507@gmail.com>
2022-02-07 13:11:26 +0200

    Add test case for login

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,4 +1,13 @@
 /// <reference types="cypress" />
 
-const URL = 'https://www.saucedemo.com/'
+const URL = "https://www.demoblaze.com/index.html";
 
+describe("Login process", () => {
+  beforeEach(() => {
+    cy.visit(URL);
+  });
+
+  it("Should be able to login with valid credentials", () => {
+    cy.get('a[data-target="#signInModal"]').click();
+  });
+});
